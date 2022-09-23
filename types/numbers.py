5a76f00d7b756455fc222a03f1883dd57124b884
Hasbik777 <sanjar.turumov@gmail.com>
2022-09-23 21:28:57 +0600

    added numbers

---
+++ types/numbers.py
@@ -0,0 +1,110 @@
+# Типы данных -> Числа int(), float()
+
+# = -> оператор присваивания
+# number = 5 # varible (переменная)
+# print(number)
+# number = 51
+# print(number)
+
+# tommorow_day -> snake case 
+# tommorowday -> Camel case
+# abc -> нижний регистр
+# ABC -> верхний регистр
+
+# +
+# a = 5
+# b = 15 
+# result = a + b
+# print(result)
+
+# print('Результат суммы чисел 5 и 15:', 5 + 15)
+
+# a = 100
+# b = 1000 
+# c = 500 #int
+# d = 56.20 #float
+# print(a + b + c + d)
+
+# - 
+# num1 = 996
+# num2 = 67.55
+# print(num2 - num1)
+
+# * 
+#  num1 = int(input('Введите число 1: '))
+#  num2 = int(input('Введите число 2: '))
+#  result = num1 * num2 
+#  print('Результат произведения чисел', num1, 'и', num2, 'равно', result)
+
+# num1 = input('Ввести число')
+# print(num1)
+# print(type(num1))
+
+# num = '15' # '15' -> 15
+# num = float(num)
+# print(num)
+# print(type(num))
+
+#  / and // and %
+# / -> Обычное деление
+# // -> Целочисленное деление(деление без остатка)
+# % -> Модульное деление(остаток от деления)
+
+# num1 = 25
+# num2 = 12
+
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+
+# ** -> возведение в степень
+ 
+# num1 = 5
+# print(num1 ** 1000) 
+
+# print (pow(2 , 5, 5)) 
+
+
+
+
+# divmod(a, b) -> Она выводит два числа, первое число это результат целочисленного деления //, а второе число это модульное деление(%)двух чисел
+
+# res = divmod(4, 5) 
+# print(res)
+
+
+# round() -> функция округления числа 
+# res = 24 / 13
+# print(round(res, 1))
+
+# abs() - абсолютное значение -> abs(-5) = 5 -> |-5| = 5
+
+# print(abs(-101))
+# print(abs(50))
+
+# import math
+# from math import pi, sqrt
+
+# print(pi) 
+# print (sqrt(25))
+
+# print(math.pi)
+
+# math.sqrt -> корень числа
+
+# print(math.sqrt(25))
+# print(math.sqrt(101))
+# print(math.sqrt(6))
+
+
+
+
+
+
+
+
+
+
+
+
+
