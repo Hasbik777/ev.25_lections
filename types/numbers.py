e2599c5e02fa9098d1374eab609a07b14bbd3b6f
Hasbik777 <sanjar.turumov@gmail.com>
2022-09-26 20:20:54 +0600

    added numbers

--- types/numbers.py
+++ types/numbers.py
@@ -31,10 +31,11 @@
 # print(num2 - num1)
 
 # * 
-#  num1 = int(input('Введите число 1: '))
-#  num2 = int(input('Введите число 2: '))
-#  result = num1 * num2 
-#  print('Результат произведения чисел', num1, 'и', num2, 'равно', result)
+# num1 = int(input('Введите число 1: '))
+# num2 = int(input('Введите число 2: '))
+# result = num1 * num2 
+
+# print('Результат произведения чисел', num1, 'и', num2, 'равно', result)
 
 # num1 = input('Ввести число')
 # print(num1)
@@ -94,15 +95,110 @@
 # print(math.sqrt(101))
 # print(math.sqrt(6))
 
+# dir() - возвращает методы обьекта или функции определенного модуля
 
+# import math
 
+# print(dir(math))
 
+"""
+Множественное приваивание
+"""
+# a = 5
 
+# a, b, c = 1, 2, 3
+# v = 5
+# n = 7
+# d = 3
 
+# v, n, d = d, v, n
+# print(v,n,d)
 
+# num1 = 10 
+# num2 = 3
+# num3 = num1
+# num1 = num2
+# num2 = num3
+# print(num1, num2)
 
+# print('Hi' * 3) # дублирование строк
 
+# str1 = '12'
+# num1 = 2
 
+# print(str1 + str(num1))
 
+"""
+Инкремент и Дикремент
+"""
+# Инкремент - увелечение значения какой либо переменной a = 1 (a += 1 -> a = a+1 )
+
+# a = 0 
+# a += 1
+# print(a)
+
+# Дикремент - уменьшение значения какой либо переменной. (a -= -> a = a - 1)
+
+# a = 0
+# a -= 1
+# print(1)
+
+# *
+# a = 5
+# a *= a
+# print(a)
+
+# /
+# a = 10
+# a /= 2
+# print(a)
+
+"""
+id() -> номер в ячейке памяти
+"""
+# a = 1
+# b = 1
+# print(id(a))
+# print(id(b))
+#  print(id(b), id(b))
+
+# type() ->  выводит тип заданной переменной
+# a = 9
+# b = 'hello'
+# print(type(a))
+# print(type(b))
+
+# var = int(input('Введите число'))
+
+# num1 = int(input('Введите число:'))
+# num2 = int(input('Введите степень:'))
+# print(num1 ** num2)
+
+'''
+Модуль random - предосьавляет функции для  генерации случайных чисел, буквы, символы
+'''
+import random
+
+# print(dir(random))
+
+# num = random.randint(11111, 99999)
+# print(num)
+
+# from random import choice
+# import string 
+
+# print(string.ascii_letters)
+# a = choice(string.ascii_letters)
+# print(a)
+
+# периметр 2 * r * pi
+# периметр pi * (r ** 2)
+
+# from math import pi
+# r = int(input('Введите радиус:'))
+# result_P = 2 * r * pi
+# result_S = pi * (r ** 2)
+# print('площадь окружность', round(result_S))
+# print('периметр окружность', round(result_P))
 
 
